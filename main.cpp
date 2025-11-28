5e9d81a0cbcd8af0173618725b1ee39152f67ce7
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2025-11-28 07:16:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main() {
   cout << "Hello World\n";
 
-      char buf[8];
-    const char* input = "this is too long";
-    strcpy(buf, input);
+  char buf[8];
+  const char* input = "this is too long";
+  strcpy(buf, input);
+  
   return 0;
 }
