a75c7baccfc4a418f3d17ba6c9891e81b69ac3fd
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2025-11-28 09:09:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,19 @@ using namespace std;
 int main() {
   cout << "Hello World\n";
 
-  char buf[8];
+  char buf[80];
   const char* input = "this is too long";
   strcpy(buf, input);
   
   return 0;
+}
+
+void oob() {
+    int arr[3];
+    arr[5] = 42; // array out of bounds
+}
+
+void uninit() {
+    int x;
+    int y = x + 2; // uninitialized
 }
